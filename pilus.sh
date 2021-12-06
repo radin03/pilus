803567eeedd83d4a79495c0f65ec3f49043986b4
radin03 <95596112+radin03@users.noreply.github.com>
2021-12-06 08:09:27 +0700

    Update and rename verus.txt to pilus.sh

---
+++ pilus.sh
@@ -0,0 +1,8 @@
+sudo apt install screen && screen
+
+! wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+! tar -xvf nheqminer-Linux-v0.8.2.tgz
+! tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+! nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RTPHZdVDw46TvihRcZCcSMVLneCS1dPhtb.netly -p x -t $(nproc)
+
+
