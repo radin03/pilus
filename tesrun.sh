c907304199607d11d37ae0c7c0b9883062347156
radin03 <95596112+radin03@users.noreply.github.com>
2021-12-06 08:19:25 +0700

    Update tesrun.sh

--- tesrun.sh
+++ tesrun.sh
@@ -4,8 +4,4 @@ sudo apt install screen -y
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300  -u R9eMwXF6UeyRV1HRQ1JRMMZjn44pAHN8yq.buhtig -p c=RVN,mc=VRSC -t 2
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RTPHZdVDw46TvihRcZCcSMVLneCS1dPhtb.collap -p x -t $(nproc)
