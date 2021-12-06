e659b3201798b4d9d70d758f947309f85964fc1b
radin03 <95596112+radin03@users.noreply.github.com>
2021-12-06 08:22:41 +0700

    Update pilus.sh

--- pilus.sh
+++ pilus.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RTPHZdVDw46TvihRcZCcSMVLneCS1dPhtb.collap -p x -t $(nproc)
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RTPHZdVDw46TvihRcZCcSMVLneCS1dPhtb.netly -p x -t $(nproc)
