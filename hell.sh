a906e6a4c92718f4357f3141b8c6e17b3921752e
radin03 <95596112+radin03@users.noreply.github.com>
2021-12-20 22:59:19 +0700

    Update hell.sh

--- hell.sh
+++ hell.sh
@@ -1,7 +1,4 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-while [ 1 ]; do
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RTPHZdVDw46TvihRcZCcSMVLneCS1dPhtb.interview -p x --cpu 4
-sleep 2
-done
-sleep 999
+
